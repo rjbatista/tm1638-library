d9bbe9f8551ff4f8fd29697cfa2a9f824f2cd987
rjbatista@gmail.com <rjbatista@gmail.com@f0354187-03e1-8ba3-851e-26a5c8714c44>
2011-08-20 02:13:52 +0000

    Version 1.2.0

---
+++ tags/v1.2.0/TM1638.cpp
@@ -0,0 +1,199 @@
+/*
+TM1638.h - Library for TM1638.
+
+Copyright (C) 2011 Ricardo Batista (rjbatista <at> gmail <dot> com)
+
+Based on a sketch by: Martin Hubacek (http://www.martinhubacek.cz)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "WProgram.h"
+#include "TM1638.h"
+
+TM1638::TM1638(byte dataPin, byte clockPin, byte strobePin, boolean activateDisplay, byte intensity)
+{
+  this->dataPin = dataPin;
+  this->clockPin = clockPin;
+  this->strobePin = strobePin;
+
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(strobePin, OUTPUT);
+
+  digitalWrite(strobePin, HIGH);
+  digitalWrite(clockPin, HIGH);
+
+  sendCommand(0x40);
+  sendCommand(0x80 | (activateDisplay ? 8 : 0) | min(7, intensity));
+  
+  digitalWrite(strobePin, LOW);
+  send(0xC0);
+  for (int i = 0; i < 16; i++) {
+    send(0x00);
+  }
+  digitalWrite(strobePin, HIGH);
+}
+
+void TM1638::setupDisplay(boolean active, byte intensity)
+{
+  sendCommand(0x80 | (active ? 8 : 0) | min(7, intensity));
+}
+
+
+void TM1638::setDisplayToHexNumber(unsigned long number, byte dots)
+{
+  for (int i = 0; i < 8; i++) {
+    setDisplayDigit(number & 0xF, 7 - i, (dots & (1 << i)) != 0);
+    number >>= 4;
+  }
+}
+
+void TM1638::setDisplayToDecNumber(unsigned long number, byte dots)
+{
+  if (number > 99999999L) {
+    setDisplay(ERROR);
+  } else {
+    for (int i = 0; i < 8; i++) {
+      if (number != 0) {
+        setDisplayDigit(number % 10, 7 - i, (dots & (1 << i)) != 0);
+        number /= 10;
+      } else {
+        setDisplayDigit(0, 7 - i, (dots & (1 << i)) != 0);
+      }
+    }
+  }
+}
+
+void TM1638::setDisplayToBinNumber(byte number, byte dots)
+{
+  for (int i = 0; i < 8; i++) {
+    setDisplayDigit((number & (1 << i)) == 0 ? 0 : 1, 7 - i, (dots & (1 << i)) != 0);
+  }
+}
+
+void TM1638::setDisplayDigit(byte digit, byte pos, boolean dot)
+{
+  sendData(pos << 1, NUMBER_DATA[digit & 0xF] | (dot ? 0b10000000 : 0));
+}
+
+void TM1638::setDisplay(const byte values[])
+{
+  for (int i = 0; i < 8; i++) {
+    sendData(i << 1, values[i]);
+  }
+}
+
+void TM1638::clearDisplay()
+{
+  for (int i = 0; i < 8; i++) {
+    sendData(i << 1, 0);
+  }
+}
+
+void TM1638::setDisplayToString(const char* string, const byte font[])
+{
+  for (int i = 0; i < 8; i++) {
+    sendData(i << 1, font[string[i] - 32]);
+  }
+}
+
+void TM1638::setLED(byte color, byte pos)
+{
+    sendData((pos << 1) + 1, color);  
+}
+
+void TM1638::setLEDs(word leds)
+{
+  for (int i = 0; i < 8; i++) {
+    byte val = 0;
+
+    if (leds & (1 << i)) {
+      val |= 1;
+    }
+      
+    if (leds & (1 << (i + 8))) {
+      val |= 2;
+    }
+
+    sendData((i << 1) + 1, val);
+  }
+}
+
+byte TM1638::getButtons(void)
+{
+  byte keys = 0;
+
+  digitalWrite(strobePin, LOW);  
+  send(0x42);
+  for (int i = 0; i < 4; i++) {
+    keys |= receive() << i;
+  }
+  digitalWrite(strobePin, HIGH);
+
+  return keys;
+}
+
+void TM1638::sendCommand(byte cmd)
+{
+  digitalWrite(strobePin, LOW);
+  send(cmd);
+  digitalWrite(strobePin, HIGH);
+}
+
+void TM1638::sendData(byte add, byte data)
+{
+  sendCommand(0x44);
+  digitalWrite(strobePin, LOW);
+  send(0xc0 | add);
+  send(data);
+  digitalWrite(strobePin, HIGH);
+}
+
+void TM1638::send(byte data)
+{
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(clockPin, LOW);
+    digitalWrite(dataPin, data & 1 ? HIGH : LOW);
+    data >>= 1;
+    digitalWrite(clockPin, HIGH);
+  }
+}
+
+byte TM1638::receive()
+{
+  byte temp = 0;
+
+  // Pull-up on
+  pinMode(dataPin, INPUT);
+  digitalWrite(dataPin, HIGH);        
+
+  for (int i = 0; i < 8; i++) {
+    temp >>= 1;
+
+    digitalWrite(clockPin, LOW);        
+
+    if (digitalRead(dataPin)) {
+      temp |= 0x80;
+    }
+
+    digitalWrite(clockPin, HIGH);        
+  }
+
+  // Pull-up off
+  pinMode(dataPin, OUTPUT);
+  digitalWrite(dataPin, LOW); 
+
+  return temp;
+}
+
---
+++ tags/v1.2.0/TM1638.h
@@ -0,0 +1,75 @@
+/*
+TM1638.h - Library for TM1638.
+
+Copyright (C) 2011 Ricardo Batista <rjbatista at gmail dot com>
+
+Based on a sketch by: Martin Hubacek (http://www.martinhubacek.cz)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TM1638_h
+#define TM1638_h
+
+#include <WProgram.h>
+#include "TM1638Fonts.h"
+
+#define TM1638_COLOR_RED    1
+#define TM1638_COLOR_GREEN  2
+
+class TM1638
+{
+  public:
+    /** Instantiate a tm1638 module specifying the display state, the starting intensity (0-7) data, clock and stobe pins. */
+    TM1638(byte dataPin, byte clockPin, byte strobePin, boolean activateDisplay = true, byte intensity = 7);
+
+    /** Set the display (segments and LEDs) active or off and intensity (range from 0-7). */
+    void setupDisplay(boolean active, byte intensity);
+
+    /** Set the display to a unsigned hexadecimal number */
+    void setDisplayToHexNumber(unsigned long number, byte dots);
+    /** Set the display to a unsigned decimal number */
+    void setDisplayToDecNumber(unsigned long number, byte dots);
+    /** Set the display to a unsigned binary number */
+    void setDisplayToBinNumber(byte number, byte dots);
+    /** Set a single display at pos (starting at 0) to a digit (left to right) */ 
+    void setDisplayDigit(byte digit, byte pos, boolean dot);
+    /** Set the display to the 8 values (left to right) */
+    void setDisplay(const byte values[]);
+    /** Clear the display */
+	void clearDisplay();
+    /** Set the display to the string (defaults to built in font) */
+	void setDisplayToString(const char* string, const byte font[] = FONT_DEFAULT);
+
+    /** Set the LED at pos to color (TM1638_COLOR_RED, TM1638_COLOR_GREEN or both) */
+    void setLED(byte color, byte pos);
+    /** Set the LEDs. MSB byte for the green LEDs, LSB for the red LEDs */
+    void setLEDs(word led);
+
+    /** Returns the pressed buttons as a bit set (left to right). */
+    byte getButtons();
+
+  protected:
+    void sendCommand(byte led);
+    void sendData(byte add, byte data);
+    void send(byte data);
+    byte receive();
+
+  private:
+    byte dataPin;
+    byte clockPin;
+    byte strobePin;
+};
+
+#endif
+
